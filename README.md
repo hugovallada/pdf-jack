25f7e48e408575d277bf9bb53734a5b177cfb61b
Hugo Vallada <valladahugo@gmail.com>
2020-08-09 12:14:17 -0300

    Adicionada as opçoes de unzip e manager na funçao merge

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 ## Funções do programa : 
 ### merge : Junta todos os arquivos pdfs em um determinado diretório
 #### Args:
+    PATH (Obrigatório) : Passa a localização do diretório/arquivo zip
     --name (Opcional) - Default = '' : Passa o nome do arquivo gerado, caso não passe, o arquivo se chamará merged.pdf
-    --safe (Opcional) - Default = False : Adiciona _merged ao fim do nome do arquivo, arquivos com _merged no nome, nunca são adicionados ao merge.
+    --safe (Opcional) - Default = False : Adiciona _merged ao fim do nome do arquivo, arquivos com _merged no nome, nunca são adicionados ao
+     merge.
+    --zip (Opcional) - Default = False : Indica se o path passado é um arquivo zip a ser descompactado
+    --manager (Opcional) - Default = False : Determina se o gerenciador de arquivos deve ser iniciado no diretório onde o merged foi gerado ou não
--- src/main.py
+++ src/main.py
@@ -11,31 +11,51 @@ app = typer.Typer()
 
 
 @app.command()
-def merge(path: str, name: Optional[str] = "", safe: Optional[bool] = False):
+# def merge(path: str, name: Optional[str] = "", safe: Optional[bool] = False):
+#     path = Path(path)
+#     merger = PdfFileMerger()
+#     for document in path.iterdir():
+#         if document.is_file() and document.suffix == ".pdf":
+#             if document.name == "merged.pdf":
+#                 continue
+#             merger.append(str(document))
+#     if name != "" and safe:
+#         name = f"{name}_merged"
+
+#     filename = "merged" if name == "" else name
+#     with open(f"{path}/{filename}.pdf", "wb") as pdf:
+#         merger.write(pdf)
+
+
+@app.command()
+def merge(
+    path: str,
+    name: Optional[str] = "",
+    safe: Optional[bool] = False,
+    zip: Optional[bool] = False,
+    manager: Optional[bool] = False,
+):
     path = Path(path)
     merger = PdfFileMerger()
+    if zip:
+        os.chdir(path.parent)
+        os.system(f"unzip {path}")
+    path = Path(str(path).replace(".zip", ""))
     for document in path.iterdir():
         if document.is_file() and document.suffix == ".pdf":
-            if document.name == "merged.pdf":
+            # if document.name == "merged.pdf":
+            if "merged" in document.name:
                 continue
             merger.append(str(document))
     if name != "" and safe:
         name = f"{name}_merged"
-
     filename = "merged" if name == "" else name
     with open(f"{path}/{filename}.pdf", "wb") as pdf:
         merger.write(pdf)
-        
-@app.command()
-def unzip_pdf(path: str, novo_path: str):
-    path = Path(path)
-    novo_path = Path(novo_path)
-    os.chdir(novo_path)
-    os.system(f"unzip {path}")
-    os.chdir(str(path).replace('.zip',''))
-    os.system('ls')
-    
-    
+
+    if manager:
+        os.chdir(path)
+        os.system("xdg-open . &")
 
 
 if __name__ == "__main__":
