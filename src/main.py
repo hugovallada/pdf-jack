099697d8d97f7b77cf92d82b6c1b100fa63e61a5
Hugo Vallada <valladahugo@gmail.com>
2020-08-09 12:17:59 -0300

    Otimizaçao no arquivo main

--- src/main.py
+++ src/main.py
@@ -11,23 +11,6 @@ app = typer.Typer()
 
 
 @app.command()
-# def merge(path: str, name: Optional[str] = "", safe: Optional[bool] = False):
-#     path = Path(path)
-#     merger = PdfFileMerger()
-#     for document in path.iterdir():
-#         if document.is_file() and document.suffix == ".pdf":
-#             if document.name == "merged.pdf":
-#                 continue
-#             merger.append(str(document))
-#     if name != "" and safe:
-#         name = f"{name}_merged"
-
-#     filename = "merged" if name == "" else name
-#     with open(f"{path}/{filename}.pdf", "wb") as pdf:
-#         merger.write(pdf)
-
-
-@app.command()
 def merge(
     path: str,
     name: Optional[str] = "",
