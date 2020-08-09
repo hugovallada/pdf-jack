a6746ea6a9ad71ef1a81f3536907fbb3251c7e6a
Hugo Vallada <valladahugo@gmail.com>
2020-08-09 01:19:23 -0300

    Iniciado o desenvolvimento da funçao de descompactaçao

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # PDF-Jack
 
-## projeto CLI para trabalhar com pdfs de maneira optmizada, salvando tempo em relação a programas com interface gráfica
+## Projeto CLI para trabalhar com pdfs de maneira optmizada, salvando tempo em relação a programas com interface gráfica
+
+## Funções do programa : 
+### merge : Junta todos os arquivos pdfs em um determinado diretório
+#### Args:
+    --name (Opcional) - Default = '' : Passa o nome do arquivo gerado, caso não passe, o arquivo se chamará merged.pdf
+    --safe (Opcional) - Default = False : Adiciona _merged ao fim do nome do arquivo, arquivos com _merged no nome, nunca são adicionados ao merge.
--- src/main.py
+++ src/main.py
@@ -2,30 +2,41 @@
 
 from pathlib import Path
 from typing import Optional
+import os
 import typer
 from PyPDF2 import PdfFileMerger
 
 
 app = typer.Typer()
 
-# pdf='~/Development/Python/Projects/pdf-jack/src/./main.py'
+
 @app.command()
-def merge(path: str, name: Optional[str] = '', safe: Optional[bool] = False):
+def merge(path: str, name: Optional[str] = "", safe: Optional[bool] = False):
     path = Path(path)
     merger = PdfFileMerger()
     for document in path.iterdir():
-        if document.is_file() and document.suffix == '.pdf':
-            if document.name == 'merged.pdf':
+        if document.is_file() and document.suffix == ".pdf":
+            if document.name == "merged.pdf":
                 continue
             merger.append(str(document))
-    if name != '' and safe:
-        name = f'{name}_merged'
+    if name != "" and safe:
+        name = f"{name}_merged"
+
+    filename = "merged" if name == "" else name
+    with open(f"{path}/{filename}.pdf", "wb") as pdf:
+        merger.write(pdf)
         
-    filename = 'merged' if name == '' else name
-    with open(f'{path}/{filename}.pdf', 'wb') as pdf:
-       merger.write(pdf)
+@app.command()
+def unzip_pdf(path: str, novo_path: str):
+    path = Path(path)
+    novo_path = Path(novo_path)
+    os.chdir(novo_path)
+    os.system(f"unzip {path}")
+    os.chdir(str(path).replace('.zip',''))
+    os.system('ls')
+    
     
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app()
