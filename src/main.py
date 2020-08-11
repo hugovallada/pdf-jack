29fc60806b5947342358ea659afd9c21561149dd
Hugo Vallada <valladahugo@gmail.com>
2020-08-11 14:37:40 -0300

    Adicionado tratamento de erros

--- src/main.py
+++ src/main.py
@@ -8,8 +8,8 @@ from PyPDF2 import PdfFileMerger
 
 
 app = typer.Typer()
+ISSUE = "https://github.com/hugovallada/pdf-jack/issues"
 
-#TODO: Tratamento de erros(FileNotFound, NotADirectoryError(Qnd passar um zip sem um --extract))
 @app.command()
 def merge(
     path: str,
@@ -30,30 +30,37 @@ def merge(
         extract_to (Optional[str], optional): Caminho do diretório onde deve ser extraido. Defaults to "".
         manager (Optional[bool], optional): Abrir o file manager. Defaults to False.
     """
-    path = Path(path)
-    merger = PdfFileMerger()
-    if extract:
-        if extract_to == '':
-            os.chdir(path.parent)
-            os.system(f'unzip {path}')
-        else:
-            os.chdir(extract_to)
-            os.system(f'unzip {path}')
-        path = Path(str(path).replace(".zip","")) if extract_to == '' else Path(f'{extract_to}/{str(path.name).replace(".zip","")}')
-    for document in path.iterdir():
-        if document.is_file() and document.suffix == ".pdf":
-            if "merged" in document.name:
-                continue
-            merger.append(str(document))
-    if name != "" and safe:
-        name = f"{name}_merged"
-    filename = "merged" if name == "" else name
-    with open(f"{path}/{filename}.pdf", "wb") as pdf:
-        merger.write(pdf)
+    try:
+        path = Path(path)
+        merger = PdfFileMerger()
+        if extract:
+            if not path.exists():
+                raise FileNotFoundError
+            if extract_to == '':
+                os.chdir(path.parent)
+                os.system(f'unzip {path}')
+            else:
+                os.chdir(extract_to)
+                os.system(f'unzip {path}')
+            path = Path(str(path).replace(".zip","")) if extract_to == '' else Path(f'{extract_to}/{str(path.name).replace(".zip","")}')
+        for document in path.iterdir():
+            if document.is_file() and document.suffix == ".pdf":
+                if "merged" in document.name:
+                    continue
+                merger.append(str(document))
+        if name != "" and safe:
+            name = f"{name}_merged"
+        filename = "merged" if name == "" else name
+        with open(f"{path}/{filename}.pdf", "wb") as pdf:
+            merger.write(pdf)
 
-    if manager:
-        os.chdir(path)
-        os.system("xdg-open . &")
+        if manager:
+            os.chdir(path)
+            os.system("xdg-open . &")
+    except (FileNotFoundError, NotADirectoryError) as err:
+        typer.echo("Não foi possível encontrar o caminho passado, verifique se não houve nenhum erro.")
+    except Exception:
+        typer.echo(f"Algo aconteceu, contate o desenvolvedor: {ISSUE}")
 
 
 if __name__ == "__main__":
